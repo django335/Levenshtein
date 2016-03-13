ea493f76f5274109cb4664871b78c75b04a60c51
Naoki Morikubo <django0820@gmail.com>
2016-03-13 16:38:03 +0900

    add xcodeproj

---
+++ Sources/Levenshtein.h
@@ -0,0 +1,19 @@
+//
+//  Levenshtein.h
+//  Levenshtein
+//
+//  Created by naoki morikubo on 2016/03/13.
+//  Copyright © 2016年 naoki morikubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Levenshtein.
+FOUNDATION_EXPORT double LevenshteinVersionNumber;
+
+//! Project version string for Levenshtein.
+FOUNDATION_EXPORT const unsigned char LevenshteinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Levenshtein/PublicHeader.h>
+
+
